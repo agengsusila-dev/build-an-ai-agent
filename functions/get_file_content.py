88869113b050a5dd3ba7a4b970ae9a42f7a83f1b
gh-ageng-susila <agengsusila@gmail.com>
2026-09-15 15:43:15 +0700

    feat: implement the get file content function by get_file_content.py

---
+++ functions/get_file_content.py
@@ -0,0 +1,29 @@
+import os
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory: str, file_path: str) -> str:
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(
+            os.path.join(working_dir_abs, file_path))
+        valid_target_dir = os.path.commonpath(
+            [working_dir_abs, target_dir]) == working_dir_abs
+
+        if valid_target_dir is False:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_dir):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(target_dir, "r") as file:
+            content = file.read(MAX_CHARS)
+            if file.read(MAX_CHARS+1):
+                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+
+        formatted_content = f"""{file_path} length: {len(content)}\n{file_path} truncated: {'truncated' in content}\n{content}\n"""
+
+        return formatted_content
+
+    except Exception as e:
+        return f"Error: {e}"
