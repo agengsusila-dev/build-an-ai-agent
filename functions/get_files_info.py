643413c289d04a14cac66ddfb34471da0953a1cd
gh-ageng-susila <agengsusila@gmail.com>
2026-09-15 13:49:11 +0700

    feat: implement feature to list file inside the working directory using functions/get_files_info.py

---
+++ functions/get_files_info.py
@@ -0,0 +1,30 @@
+import os
+
+
+def get_files_info(working_directory: str, directory: str = ".") -> str:
+    print(f"Result of '{directory}' directory:")
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
+        valid_target_dir = os.path.commonpath(
+            [working_dir_abs, target_dir]) == working_dir_abs
+
+        if valid_target_dir is False:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        list_directory = os.listdir(target_dir)
+        list_file_specs_in_directory = []
+        for file in list_directory:
+            file_path = os.path.join(target_dir, file)
+            list_file_specs_in_directory.append(
+                f"- {file}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)}")
+
+        list_file_specs_in_directory_string = "\n".join(
+            list_file_specs_in_directory)
+
+        return list_file_specs_in_directory_string
+
+    except Exception as e:
+        return f"Error: {e}"
